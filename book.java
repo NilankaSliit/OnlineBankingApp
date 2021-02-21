c83f42567de0b64ca6159669d349be09e703f327
NilankaSliit <79305240+NilankaSliit@users.noreply.github.com>
2021-02-21 23:08:10 +0530

    Add new class book
    add new class book

---
+++ book.java
@@ -0,0 +1,14 @@
+public class book{
+	boolean addBook(){
+		//Add code
+		return true;
+	}
+	boolean updateBook(){
+		//Add code
+		return true;
+	}
+	boolean deleteBook(){
+		//Add code
+		return true;
+	}
+}
